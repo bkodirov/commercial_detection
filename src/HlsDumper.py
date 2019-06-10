b49206e536be7e9b9ec81b0b9152808a9f8684a3
bkodirov <bkodirov1986@gmail.com>
2019-06-10 00:13:35 -0400

    Dumper can work with variant and master as well

--- src/HlsDumper.py
+++ src/HlsDumper.py
@@ -1,10 +1,14 @@
 import glob
-from os import scandir
-from urllib import request, parse
+from urllib import parse
+
 import m3u8
 import os
 
+import requests
+
 from src.demuxer import transportation_segment_demux
+
+__http_session__ = requests.Session()
 
 BASE_EXAMPLES_PATH = os.path.abspath(os.path.join(os.getcwd(), '..', 'examples/'))
 
@@ -24,22 +28,36 @@ def main():
         print('----')
 
 
+def create_media_playlist_url(base_url, second_url):
+    if bool(parse.urlparse(second_url).netloc):
+        return second_url
+    else:
+        result = parse.urljoin(base_url, second_url)
+        parse.urlparse(result)
+        return result
+
+
 def download_hls_files(folder_to_store, file_prefix, base_url, variant_hls):
     print("Started downloading playlist form ", base_url, "to", folder_to_store)
     for file in variant_hls.files:
-        file_uri = parse.urljoin(base_url, file)
+        file_uri = create_media_playlist_url(base_url, file)
         print("fetching file", file_uri)
         # TODO Handle result properly
         file_destination = os.path.join(folder_to_store, file_prefix + os.path.split(file)[1])
         print("final_destination ", file_destination)
-        request.urlretrieve(file_uri, file_destination)
+        with __http_session__.request('GET', file_uri) as response:
+            with open(file_destination, 'wb') as local_copy:
+                local_copy.write(response.content)
 
 
 def retrieve_m3u8_object(base_url, uri):
     # TODO handle uri. It might be relative or absolute path
-    variant_url = parse.urljoin(base_url, uri)
-    with request.urlopen(variant_url) as response:
-        variant_data = response.read().decode('utf-8')
+    try:
+        variant_url = parse.urljoin(base_url, uri)
+    except Exception:
+        print("Can not join", base_url, uri)
+    with __http_session__.request('GET', variant_url) as response:
+        variant_data = response.text
     # TODO Handle result properly
     print('---------------------')
     print(variant_data)
@@ -74,24 +92,24 @@ def dump_stream(folder_name, url):
         if not media_item or media_item.autoselect:
             desired_audio_playlist = media_item
 
-    high_bitrate_video_playlist = None
+    high_bitrate_video_playlist_uri = None
     if not hls_playlist.is_variant:
-        for variant_playlist_obj in hls_playlist.playlists:
-            if not high_bitrate_video_playlist or variant_playlist_obj.stream_info.bandwidth > high_bitrate_video_playlist.stream_info.bandwidth:
-                high_bitrate_video_playlist = variant_playlist_obj
+        hls_playlist.playlists.sort(key=lambda playlist: playlist.stream_info.bandwidth)
+        high_bitrate_video_playlist_uri = hls_playlist.playlists[-1].uri
     else:
-        high_bitrate_video_playlist = hls_playlist
+        high_bitrate_video_playlist_uri = url
 
     output_folder = os.path.join(stream_folder, "output")
     os.makedirs(output_folder, exist_ok=True)
     # Store audio on the output folder
-    generate_raw_stream(url, desired_audio_playlist.uri, "audio_", output_folder)
-
+    if desired_audio_playlist and desired_audio_playlist.uri:
+        generate_raw_stream(url, desired_audio_playlist.uri, "audio_", output_folder)
+    else:
+        print("There is no independent audio playlist")
     # Store audio on the output folder
-    generate_raw_stream(url, high_bitrate_video_playlist.uri, "video_", output_folder)
+    generate_raw_stream(url, high_bitrate_video_playlist_uri, "video_", output_folder)
 
 
 if __name__ == "__main__":
-    dump_stream("akamai2",
-                'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8')
-    # dump_stream("fubo_hangoverIII", 'https://playlist-nonlive.fubo.tv/v4/finite.m3u8?hdnts=ip%3D74.90.241.179~st%3D1559885899~exp%3D1559972299~acl%3D%2F%2A~data%3D1559972299~hmac%3D61bd952f2c66028bc8ef837f5251bcb2584ad57f6bb9b2238640d359aca65a7c&watchToken=eyJhbGciOiJIUzI1NiIsInRva2VuVHlwZSI6IlVzYUZpbml0ZVdhdGNoVG9rZW4iLCJ0eXAiOiJKV1QifQ.eyJ3YXRjaFRva2VuIjp7ImNhbGxTaWduIjoiQU1DIiwiZGV2aWNlIjoiQUxMIiwiYmFuZHdpZHRoIjoiTUFTVEVSIiwiY291bnRyeSI6IlVTQSIsInVzZXJJZCI6ImJla2FAZnViby50diIsInJlcXVlc3RUeXBlIjoiZHZyIiwicmVnaW9uIjoid2VzdCIsInN0YXJ0IjoxNTU2MjIwNjAwLCJlbmQiOjE1NTYyMjc4NjB9LCJob21lWmlwIjoiMTAwMTkiLCJnZW9aaXAiOiIxMTIyNiJ9.vGddSHRqS13Oz2b4wpD0FRkPPQ2MHzbH5A4KTBJOOqU')
+    # dump_stream("akamai2", 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8')
+    dump_stream("long", 'https://dvr.fubo.tv/2019/04/24/N0347/010000-020100/media-8872332-sorted.m3u8?hdnts=ip%3D74.90.241.179~st%3D1560135065~exp%3D1560221465~acl%3D%2F%2A~data%3D1560221465~hmac%3D0dcfd96488c74331281f4023117e4682864a1b6ddde952d5654ebbfbf0e1c118')
